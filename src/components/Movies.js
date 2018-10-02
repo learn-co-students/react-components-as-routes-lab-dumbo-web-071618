96c266d3241e216a3712b7627e04d71ebc69ec28
joalorro <joalorro@gmail.com>
2018-10-02 08:50:43 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderGenres = (movie) => {
+	return movie.genres.map(genre => {
+		return (
+			<li>{genre}</li>
+		)
+	})
+}
+
+const renderMovies = () => {
+	return movies.map(movie => {
+		return (
+			<div>
+				<h1>{movie.title}</h1>
+				<h3>{movie.time}</h3>
+				<ul>
+					{renderGenres(movie)}
+				</ul>
+			</div>
+		)
+	})
+}
+
 const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Movies Page</h1>
+			{renderMovies()}
+		</div>
+	);
 };
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div className="navbar">
+			<NavLink to="/" activeStyle={{background: 'darkblue'}} >Home</NavLink>
+			<NavLink to="/movies" activeStyle={{background: 'darkblue'}} >Movies</NavLink>
+			<NavLink to="/directors" activeStyle={{background: 'darkblue'}} >Directors</NavLink>
+			<NavLink to="/actors" activeStyle={{background: 'darkblue'}} >Actors</NavLink>
+		</div>
+	);
 };
 
 export default NavBar;
